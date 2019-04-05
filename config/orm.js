3d653c9b1f4fc1b59a2db94bf389abf393916c0b
Namita Shenai <namita.shenai@gmail.com>
2019-04-05 00:05:23 -0700

    Added SQL Injections in the orm files

--- config/orm.js
+++ config/orm.js
@@ -3,6 +3,54 @@ var connection = require("../config/connection.js");
 
 // Object for all our SQL statement functions.
 var orm = {
+    selectAll: function(tableInput, cb) {
+        var queryString = "SELECT * FROM " + tableInput + ";";
+        connection.query(queryString, function(err, result) {
+          if (err) {
+            throw err;
+          }
+          console.log(results);
+          cb(result);
+        });
+      },
+      insertOne: function(table, cols, colValue, cb) {
+        var queryString = "INSERT INTO " + table;
+    
+        queryString += " (";
+        queryString += cols.toString();
+        queryString += ") ";
+        queryString += "VALUES (' " + colValue + " ') ";
+    
+        console.log(queryString);
+    
+        connection.query(queryString, vals, function(err, result) {
+          if (err) {
+            throw err;
+          }
+    
+          cb(result);
+        });
+    },
+    updateOne: function(tableName, values, cb) {
+        var queryString = "UPDATE ";
+  
+        if(values.complete === "0") {
+           queryString += tableName;
+           queryString += " SET complete=" + true;
+           queryString += " WHERE id=" + values.id;
+        } else {
+           queryString += tableName;
+           queryString += " SET complete=" + false;
+           queryString += " WHERE id=" + values.id;
+        }
+        
+        console.log(queryString);
+
+        connection.query(queryString, function(err, result) {
+           if (err) throw err;
+           cb(result);
+        });
+     },
 }
 
 // Export the orm object for the model (cupcake.js).
---
+++ models/cupcake.js
@@ -0,0 +1,12 @@
+// ---------------------------------------------------
+// Dependencies
+// --------------------------------------------------- 
+
+var orm = require("../config/orm.js");
+
+var cupcake ={
+
+}; 
+
+// Export the database functions for the controller (cupcakecontroller.js).
+module.exports = cupcake;
